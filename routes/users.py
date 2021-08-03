cadf414363e603511415f7345bef3a7bda24658b
JahunSeo <wkgjs0809@gmail.com>
2021-08-03 22:56:51 +0900

    [johnny] users rebuild check

--- routes/users.py
+++ routes/users.py
@@ -4,7 +4,6 @@ from . import routes
 from flask_jwt_extended import *
 import bcrypt
 
-
 from app import db
 
 @routes.route("/api/user/login", methods=["POST"])
@@ -41,67 +40,34 @@ def user_login():
 
 
 
-# @routes.route("/api/user/register", methods=["POST"])
-# def user_register():
-#    try:
-#       # 데이터 확인하기
-#       formdata = request.get_json()
-#       formdata = request.form # temp
-#       username = formdata.get("username")
-#       password = formdata.get("password")
-#       if not username:
-#          raise Exception("유저 이름이 없습니다.")
-#       if not password:
-#          raise Exception("유저 비밀번호가 없습니다.")
-#       # 비밀번호 암호화하기
-#       hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-#       hashed_password = hashed_password.decode('utf-8')
-#       # 이미 동일한 username이 있는지 확인
-#       prev = db.users.find_one({"username": username})
-#       if prev:
-#          raise Exception("이미 동일한 이름의 유저가 있습니다.")
-#       # 저장할 정보 구성하기
-#       user = {}
-#       user["username"] = username
-#       user["password"] = hashed_password
-#       # 저장하기
-#       db.users.insert_one(user)
-#       return jsonify({"status": 200, "msg": "사용자 등록하기 성공"})
-#    except Exception as e:
-#       return jsonify({"status": 500, "msg": str(e)})
-
-
-# @routes.route("/api/user/login", methods=["POST"])
-# def user_login():
-#    try:
-#       # 데이터 확인하기
-#       formdata = request.get_json()
-#       formdata = request.form # temp
-#       username = formdata.get("username")
-#       password = formdata.get("password")
-#       if not username:
-#          raise Exception("유저 이름이 없습니다.")
-#       if not password:
-#          raise Exception("유저 비밀번호가 없습니다.")
-#       # 유저 찾기
-#       user = db.users.find_one({"username": username})
-#       if user is None:
-#          raise Exception("잘못된 유저 이름입니다.")
-#       # 비밀번호 체크하기
-#       check_pw = bcrypt.checkpw(password.encode('utf-8'), user["password"].encode('utf-8'))
-#       print(username, password, check_pw)
-#       if not check_pw:
-#          raise Exception("잘못된 비밀번호입니다.")
-#       payload = {
-#          "username": username,
-#          "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=30)
-#       }
-#       token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
-#       return jsonify({"status": 200, "msg": "사용자 로그인 성공", "token": token})
-
-#    except Exception as e:
-#       return jsonify({"status": 500, "msg": str(e)})
-
+@routes.route("/api/user/register", methods=["POST"])
+def user_register():
+   try:
+      # 데이터 확인하기
+      formdata = request.get_json()
+      formdata = request.form # temp
+      username = formdata.get("username")
+      password = formdata.get("password")
+      if not username:
+         raise Exception("유저 이름이 없습니다.")
+      if not password:
+         raise Exception("유저 비밀번호가 없습니다.")
+      # 비밀번호 암호화하기
+      hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+      hashed_password = hashed_password.decode('utf-8')
+      # 이미 동일한 username이 있는지 확인
+      prev = db.users.find_one({"username": username})
+      if prev:
+         raise Exception("이미 동일한 이름의 유저가 있습니다.")
+      # 저장할 정보 구성하기
+      user = {}
+      user["username"] = username
+      user["password"] = hashed_password
+      # 저장하기
+      db.users.insert_one(user)
+      return jsonify({"status": 200, "msg": "사용자 등록하기 성공"})
+   except Exception as e:
+      return jsonify({"status": 500, "msg": str(e)})
 
 # @routes.route("/api/user/check", methods=["GET"])
 # def user_check():
