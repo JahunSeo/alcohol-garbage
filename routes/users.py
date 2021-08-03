695ebb3bac0a28a7041b99240f7f1fd883995f76
JahunSeo <wkgjs0809@gmail.com>
2021-08-03 19:19:53 +0900

    [johnny] add api user check

--- routes/users.py
+++ routes/users.py
@@ -67,3 +67,18 @@ def user_login():
 
    except Exception as e:
       return jsonify({"status": 500, "msg": str(e)})
+
+
+@routes.route("/api/user/check", methods=["GET"])
+def user_check():
+    try:
+        token = request.headers["token"]
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        user = db.users.find_one({"username": payload["username"]})
+        if user is None:
+            raise Exception("토큰으로 정보를 확인할 수 없습니다.")
+        return jsonify({"status": 200, "msg": "사용자 체크 성공", "username": user["username"]})
+    except jwt.ExpiredSignatureError:
+        return jsonify({"status": 500, "msg": "로그인 시간 만료"})
+    except Exception as e:
+      return jsonify({"status": 500, "msg": str(e)})
