19bc7705bafe3c0d7905a6782ac9ead03d5c8229
JahunSeo <wkgjs0809@gmail.com>
2021-08-03 14:51:44 +0900

    [johnny] update app.py's import part

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-app = Flask(__name__)
-
+from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbloginSystem2
-
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
-SECRET_KEY = 'secret'
-
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
+from datetime import datetime
+from bson.objectid import ObjectId
 import jwt
 
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
-import datetime
+import xml.etree.ElementTree as elemTree
+
+# parse XML
+try:
+   tree = elemTree.parse("keys.xml")
+   SECRET_KEY = tree.find('string[@name="AUTH_SECRET_KEY"]').text
+   MONGO_DB_URL = tree.find('string[@name="MONGO_DB_URL"]').text
+except:
+   print("ERROR!!! check your 'keys.xml' file")
+   SECRET_KEY = "test1234"
+   MONGO_DB_URL = "localhost"
+
+# print("** SECRET_KEY **", SECRET_KEY)
+# print("** MONGO_DB_URL **", MONGO_DB_URL)
+
+app = Flask(__name__)
+client = MongoClient(MONGO_DB_URL, 27017)
+db = client.beerdb
+
+
+###########################################################
+
 
 # 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
 # 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
