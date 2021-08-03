dd49ec90f38acacfb5bf36cc8fce2a389e0ce734
JahunSeo <wkgjs0809@gmail.com>
2021-08-04 00:39:02 +0900

    [johnny] rebuild abv as float

--- crawler.py
+++ crawler.py
@@ -15,18 +15,23 @@ base_url = "https://www.ratebeer.com"
 url = base_url + "/top"
 headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
 
-# data = requests.get(url, headers=headers)
-# soup = BeautifulSoup(data.text, 'html.parser')
+data = requests.get(url, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
 
-# rows = soup.select("#main-content > table > tbody > tr")
-# for row in rows:
-#     atag = row.select_one("td:nth-child(2) > a")
+rows = soup.select("#main-content > table > tbody > tr")
+for row in rows:
+    atag = row.select_one("td:nth-child(2) > a")
 
-#     beer = {}
-#     beer["name"] = atag.text
-#     beer["suburl"] = atag["href"]
-#     beer["abv"] = row.select_one("td:nth-child(4)").text.replace("%", "")
-#     print(beer)
+    beer = {}
+    beer["name"] = atag.text
+    beer["suburl"] = atag["href"]
+    abv = row.select_one("td:nth-child(4)").text.replace("%", "")
+    beer["abv"] = float(abv)
+    print(beer)
 
-#     db.beers.insert_one(beer)
+    db.beers.insert_one(beer)
 
+
+beers = db.beers.find({}, {"_id": False})
+beers = list(beers)
+print(beers)
