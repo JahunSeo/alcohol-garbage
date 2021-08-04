199f5b9395e807f6dbb1a3da86fa64f7effbf901
JahunSeo <wkgjs0809@gmail.com>
2021-08-04 18:15:00 +0900

    [johnny] crawling ing

--- crawler.py
+++ crawler.py
@@ -6,24 +6,47 @@ import xml.etree.ElementTree as elemTree
 
 tree = elemTree.parse("keys.xml")
 MONGO_DB_URL = tree.find('string[@name="MONGO_DB_URL"]').text
+MONGO_DB_URL = "localhost"
 
 client = MongoClient(MONGO_DB_URL, 27017)
 db = client.beerdb
 
-# url = "http://www.beerforum.co.kr/index.php?mid=beer_tastingreply"
-base_url = "https://www.beeradvocate.com/beer"
-url = base_url + "/top-rated"
+url = "http://www.beerforum.co.kr/index.php?mid=beer_tastingreply"
 headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
 
 
 data = requests.get(url, headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
-rows = soup.select("#ba-content > table > tbody > tr")
+
+rows = soup.select("tbody > tr")
 print(len(rows))
-rows = rows[1:]
 for row in rows:
-    atag = row.select_one("td:nth-child(2) > a > b")
+    beer = {}
+    atag = row.select_one("td:nth-child(1) > a")
+    # 링크
+    beer["url"] = atag["href"]
+    print(beer["url"])
+    subdata = requests.get(beer["url"], headers=headers)
+    subsoup = BeautifulSoup(subdata.text, 'html.parser')
+    table = subsoup.select_one("div.boardRead")
+    table = table.select_one("table table")
+
+    abv = table.select_one("tr:nth-child(2) > td > span").text
+    beer["abv"] = abv
+
+    print(beer)
+    break
+
+    # # 이름
+    # names = atag.text
+    # names = names.split("/")
+    # beer["original_name"] = names[0].strip()
+    # beer["name"] = names[1].strip()
+    # # 국가
+    # beer["country"] = row.select_one("td:nth-child(3)").text
+    # print(beer)
+    break
 
 
     beer = {}
@@ -39,18 +62,3 @@ for row in rows:
     break
 
     # db.beers.insert_one(beer)
-
-
-# beers = db.beers.find({}, {"_id": False})
-# beers = list(beers)
-# for beer in beers:
-#     url = base_url + beer["suburl"]
-#     data = requests.get(url, headers=headers)
-#     soup = BeautifulSoup(data.text, 'html.parser')
-#     print(url)
-
-#     imgtag = soup.select_one("img")
-#     image = imgtag["src"]
-#     print(image)
-#     result = db.beers.update_one({"name": beer["name"]}, {"$set": {"image": image}})
-#     break
