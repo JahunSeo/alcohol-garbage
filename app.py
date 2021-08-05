21382ca3b6ebb105dbe70ff2f69952621cecc800
JahunSeo <wkgjs0809@gmail.com>
2021-08-05 09:50:13 +0900

    [johnny] add @app.after_request to refresh expiring jwt

--- app.py
+++ app.py
@@ -53,6 +53,24 @@ def my_expired_token_callback(jwt_header, jwt_payload):
     unset_jwt_cookies(resp)
     return resp
 
+
+# Using an `after_request` callback, we refresh any token that is within 30
+# minutes of expiring. Change the timedeltas to match the needs of your application.
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.datetime.now(datetime.timezone.utc)
+        target_timestamp = datetime.datetime.timestamp(now + datetime.timedelta(seconds=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+
 @app.route("/")
 @jwt_required(optional=True)
 def main():
